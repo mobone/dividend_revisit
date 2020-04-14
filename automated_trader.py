339e3f1d58e121fe567e836ce89a06977d6f36f0
mobone <nicholas.reichel@gmail.com>
2020-04-14 08:42:28 -0500

    Added automated trader. need to add sell side

---
+++ automated_trader.py
@@ -0,0 +1,190 @@
+
+
+import alpaca_trade_api as tradeapi
+from math import floor
+import logging
+from time import sleep
+import pandas as pd
+from threading import Thread
+from datetime import datetime
+import requests_cache
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.chrome.options import Options
+from multiprocessing.pool import ThreadPool
+
+import sqlite3
+import warnings
+
+warnings.simplefilter('ignore')
+
+conn = sqlite3.connect('dividends.db')
+
+
+
+logging.basicConfig(format='%(asctime)s %(message)s')
+logging.basicConfig(filename='./trader.log', level=logging.INFO)
+
+class automated_trader():
+    def __init__(self):
+
+        logging.info('started automated trader')
+
+        self.api = tradeapi.REST(
+                                'PKU23YDAWZVS4PUESHQ6',
+                                'dqkM6qqmUbkAuWtnzcwg7wrsUOmp8hNn80dnyGh9',
+                                'https://paper-api.alpaca.markets'
+                                )
+
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('log-level=3')
+        self.driver = webdriver.Chrome(options=chrome_options, executable_path='./chromedriver.exe')
+        self.delay = 10
+
+        self.get_dividend_plays()
+        self.make_new_trades()
+
+    def get_dividend_plays(self):
+        
+        dividend_dfs = []
+        url = 'https://www.dividend.com/ex-dividend-dates/#tm=3-ex-div-dates&r=Webpage%231280&f_22_from=next_business_day&f_22_to=next_business_day&only=meta%2Cdata%2Cthead&sort_by=latest_yield&sort_direction=desc'
+        self.driver.get(url)
+        while True:
+            try:
+                WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, 'Next ')))
+                next_element = self.driver.find_element_by_link_text("Next ›")
+
+                
+                
+
+                dividend_page_df = pd.read_html(self.driver.page_source)[0]
+                #print(dividend_page_df)
+
+                dividend_dfs.append(dividend_page_df)
+                
+        
+
+                next_element.click()
+            except Exception as e:
+                #print(e)
+                break
+                
+
+
+        dividends = pd.concat(dividend_dfs)
+        
+        del dividends['Unnamed: 0']
+        del dividends['DARS™ Rating']
+        
+        for col_name in ['Yield', 'Stock Price', 'Div Payout']:
+        
+
+            dividends[col_name] = dividends[col_name].str.replace(',', '')
+            dividends[col_name] = dividends[col_name].str.replace('$', '')
+            dividends[col_name] = dividends[col_name].str.replace('%', '')
+            dividends[col_name] = dividends[col_name].astype(float)
+        dividends['Yield'] = dividends['Yield'] / 100
+
+        #dividends['Ex-Div Date'] = pd.to_datetime(dividends['Ex-Div Date'])
+        
+        dividends['Div Payout Percent'] = dividends['Div Payout'] / dividends['Stock Price']
+
+        dividends = dividends.sort_values(by=['Div Payout Percent'], ascending=False)
+        
+        
+        dividends['Stock Symbol']= dividends['Stock Symbol'].str.split("-", n = 1, expand = True) 
+        dividends = dividends.drop_duplicates(subset=['Stock Symbol'])
+        dividends = dividends.set_index('Stock Symbol')
+
+        dividends = dividends[ dividends['Div Payout Percent'] > .01 ]
+
+        print('Trading these dividends')
+        print(dividends)
+        
+        self.dividends = dividends
+
+
+    def make_new_trades(self):
+        order_queue = []
+        for symbol, dividend_row in self.dividends.iterrows():
+            symbol = symbol.split('-')[0]
+            
+            price = dividend_row['Stock Price']*1.1
+            order_queue.append( (symbol, 'buy', price) )
+            #order_status, fill_price, order_id = self.submit_order(symbol, 'buy', price)
+        
+        t_pool = ThreadPool(5)
+        orders = t_pool.map(self.submit_order, order_queue)
+        
+        
+        for order_result in orders:
+            order_status, fill_price, order_id, symbol = order_result
+        
+            dividend_row = self.dividends.loc[symbol]
+            dividend_row['buy_date'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            dividend_row['buy_order_status'] = order_status
+            dividend_row['buy_fill_price'] = fill_price
+            dividend_row['buy_order_id'] = order_id
+            
+            dividend_row['sell_date'] = None
+            dividend_row['sell_order_status'] = None
+            dividend_row['sell_fill_price'] = None
+            dividend_row['sell_order_id'] = None
+            dividend_row['stock_roi'] = None
+
+            dividend_row['unique_id'] = symbol+'_'+str(dividend_row['Ex-Div Date'])
+            dividend_row = dividend_row.to_frame().T
+
+            for col_name in dividend_row.columns:
+                dividend_row[col_name] = dividend_row[col_name].apply(pd.to_numeric, errors='ignore')
+            
+            dividend_row.to_sql('trades', conn, if_exists='append')
+        
+
+    def submit_order(self, params):
+        symbol, side, limit_price = params
+        order_status = None
+        fill_price = None
+        order_id = None
+        try:
+            order = self.api.submit_order( 
+                                    symbol=symbol, 
+                                    qty = 1, 
+                                    side = side, 
+                                    type = 'limit', 
+                                    time_in_force = 'day',
+                                    extended_hours = True,
+                                    limit_price = float(round(limit_price, 2))
+                                    )
+            print('order submitted', symbol, side, limit_price, order.id)
+            order_id = order.id
+            for _ in range(10):
+                sleep(1)
+                order = self.api.get_order(order.id)
+                order_status = order.status
+                
+                if order.status == 'filled':
+                    print('order filled', symbol, side, limit_price, order.id)
+                    fill_price = float(order.filled_avg_price)
+                    
+                    break
+        except Exception as e:
+            order_status = str(e)
+            print('got exception', e)
+        
+        if order_id is not None and order.status != 'filled':
+            print('not filled. Cancelling order')
+            self.api.cancel_order(order_id)
+            
+            order_status = 'cancelled'
+        
+        return order_status, fill_price, order_id, symbol
+
+
+automated_trader()
--- div_simulator.py
+++ div_simulator.py
@@ -1,59 +1,115 @@
 import pandas as pd
 
 import numpy as np
-
-def run_simulation(cutoff, end_cutoff, num_trades):
+from itertools import product
+from multiprocessing import Pool
+import warnings
+warnings.simplefilter("ignore")
+def run_simulation(params):
+    #print(params)
+    cutoff, num_trades, start_balance, strat, max_shares = params
     df = pd.read_csv('simulation_input_6y.csv')
     df = df.dropna()
     dfs = df.groupby(by=['start_date'])
-    balance = 10000
+    balance = start_balance
     total_num_trades = 0
+    total_wins = 0
+    avg_prices = []
+    
     for start_date, df in dfs:
 
         df['div_percent'] = df['div_amount'] / df['start_price']
 
         df = df[ df['div_percent'] > cutoff ]
-        df = df[ df['div_percent'] < end_cutoff ]
+        #df = df[ df['div_percent'] < end_cutoff ]
 
         trades = df
 
-        if len(trades)>num_trades:
+        if len(trades)>num_trades and strat == 'random':
             trades = df.sample(n=num_trades)
-
+        elif len(trades)>num_trades and strat == 'sorted':
+            df = df.sort_values(by=['div_percent'])
+            trades = df.tail(num_trades)
+        
+        if trades.empty:
+            continue
         avg_price = trades['start_price'].mean()
+        avg_prices.append(avg_price)
 
-        balance_to_use = balance * (1/num_trades)
-
+        balance_to_use = np.floor(balance * (1/ len(trades) ))
+        #if balance_to_use>10000:
+            #balance_to_use = 10000
+        #print(start_date, balance)
+        
         for _, trade in trades.iterrows():
 
             #print(trade)
             try:
 
-                num_shares = int( balance_to_use / float(trade['start_price']) )
+                num_shares = np.floor( balance_to_use / float(trade['start_price']) )
+                if num_shares == 0:
+                    continue
+                if num_shares>max_shares:
+                    #print('max shares', balance_to_use, float(trade['start_price']))
+                    num_shares = max_shares
 
-                #print(num_shares)
-
-
+                
 
                 #share_profit = num_shares * (float(trade['end_price']) - float(trade['start_price']))
                 dividend_profit = num_shares * float(trade['div_amount'])
-
-                balance = balance - (num_shares * float(trade['start_price']))
-                balance = balance + (num_shares * float(trade['end_price'])) + dividend_profit
+                investment_cost = (num_shares * float(trade['start_price']))
+                investment_return = (num_shares * float(trade['end_price']))
+                #print(trade)
+                #print(start_date, num_shares, investment_cost, investment_return, dividend_profit)
+                
+                balance = balance - investment_cost
+                
+                balance = balance + investment_return + dividend_profit
+                #print(balance)
+                #input()
 
             except Exception as e:
-                print(e)
+                #print(e)
                 pass
+            #input()
+
+            if (investment_return - investment_cost + dividend_profit) > 0:
+                total_wins = total_wins + 1
+            
             total_num_trades = total_num_trades + 1
+    try:
+        avg_price_per_share = sum(avg_prices) / len(avg_prices) 
+    except:
+        avg_price_per_share = None
+    #input()
     #print(balance)
     #print(num_trades)
-    return [ cutoff, end_cutoff, num_trades, total_num_trades,  balance, (balance - 10000) / 10000 ]
-results = []
-for cutoff in [.005, .01, .015, .02]:
-    for end_cutoff in [.01, .015, .025, .03, .035, 2]:
-        for num_trades in [5,10,15]:
-            result = run_simulation(cutoff, end_cutoff, num_trades)
-            results.append(result)
-            result_df = pd.DataFrame(results, columns = ['cutoff', 'end_cutoff', 'num_trades', 'total_num_trades', 'balance', 'percent_change'])
-            print(result_df.sort_values(by=['percent_change']))
-            result_df.to_csv('div_results.csv')
+    win_rate = total_wins / float(total_num_trades)
+    return [ cutoff, start_balance, num_trades, strat, max_shares, win_rate, total_num_trades, avg_price_per_share, balance, (balance - start_balance) / start_balance ]
+if __name__ == "__main__":
+    
+    params = []
+    results = []
+    cutoffs = [.005, .01, .015, .02, .025]
+    #end_cutoff = [.035, 2]
+    num_trades = [3,5,10,15]
+    start_balance = [2000]
+    strat = ['sorted']
+    max_shares = [100,200,300,400]
+    p = Pool(12)
+    all_results = p.map(run_simulation, product( cutoffs, num_trades, start_balance, strat, max_shares ) )
+
+    for result in all_results:
+                    #result = run_simulation(cutoff, num_trades, start_balance)
+                    #results.append(result)
+        results.append(result)
+    result_df = pd.DataFrame(results, columns = ['cutoff', 'start_balance', 'num_trades', 'strat', 'max_shares', 'win_rate', 'total_num_trades', 'avg_price_per_share', 'balance', 'percent_change'])
+    print(result_df.sort_values(by=['balance']))
+    result_df.to_csv('div_results.csv')
+    print( result_df.groupby(by=['max_shares','num_trades'])['percent_change'].mean() )
+    """
+    # 0.020  2.000  500  1  1171  38.309430  89010.710714  177.021421
+    params = [.02,2,1,500, 'sorted']
+    run_simulation(params)
+    """
+    
